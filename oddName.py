071c2d92cb2bff70c321bb9ec9f493b4cd8dc572
jc463652 <sam.sheppard@my.jcu.edu.au>
2017-08-16 13:43:16 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,12 @@
 """Sam Sheppard"""
+
+
+name = input(str("What is your name? "))
+while name == "":
+    print("That is not valid")
+    name = input(str("What is your name? "))
+AlternateName = " "
+for i in range(len(name)):
+    if (i % 2) == 0:
+        AlternateName += name[i]
+print(AlternateName)
